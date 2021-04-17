46c0d2e2c577b549b16140a10dbde4e525fd0318
Jishnu Raj <jishnu.r@giconsulting.com>
2021-04-17 23:04:13 +0530

    Installed bootstrap

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h3>Installing BOOTSTRAP</h3>
+   <button type="button" class="btn btn-primary">Primary</button>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
